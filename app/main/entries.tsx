cbec50c0c670bb3652f4d223dd1ee021b84a8c27
wahyuditya <wahyudityapersonal@gmail.com>
2025-01-05 16:12:33 +0700

    fixed mobile view prt2

--- app/main/entries.tsx
+++ app/main/entries.tsx
@@ -60,7 +60,11 @@ function Entries() {
         }  fixed bottom-0 left-0 right-0 bg-[linear-gradient(to_right_top,#fffbc5,#fffcce,#fffdd8,#fffee1,#ffffea)] border border-appColor-border transition-all duration-300 ease-in-out`}
       >
         <div className="logs px-[18px]">
-          <div className="top h-[71px] mb-[32px] relative justify-center flex items-center w-full">
+          <div
+            className={`top h-[71px] mb-[32px] relative justify-center flex items-center w-full ${
+              isOpen ? "mt-[50px] md:mt-[0px]" : ""
+            }`}
+          >
             <div className="title text-center font-playfair font-medium text-[26px] text-appColor-950">
               My Gratitude
             </div>
